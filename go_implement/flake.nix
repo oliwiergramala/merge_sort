1d45d17a8e90444bfceda1f4a3681cc233063cae
Oliwier <olivier.gramala@gmail.com>
2026-03-17 11:40:55 +0100

    chore/add_directorys

---
+++ go_implement/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        go
+        gopls
+        delve
+        gotools
+        go-tools
+        golangci-lint
+      ];
+
+      shellHook = ''
+        echo "Go development environment ready"
+        go version
+      '';
+    };
+  };
+}
